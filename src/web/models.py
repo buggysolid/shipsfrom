19c0092d02c9e7e815452445b178e1f376dd1e49
Ciaran <>
2023-09-01 21:47:17 +0100

    Basic shop detail model.

--- src/web/models.py
+++ src/web/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+class Shop(models.Model):
+    # Excessively large length but you never know.
+    # https://www.kalzumeus.com/2010/06/17/falsehoods-programmers-believe-about-names/
+    category = models.CharField(max_length=200)
+    shipper = models.CharField(max_length=200)
+    website = models.URLField()
