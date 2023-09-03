6e032b193a2062d85dc3803dff378d2f472fce83
Ciaran <>
2023-09-03 12:29:21 +0100

    Add basic template for shops.
    Get the basic flow of
    
    load from database
    prepare for view
    send to view
    render via template
    
    setup so I can iterate on it from there.
    
    For now all this does is load all the Shop objects from the database and access .website attribute.

--- src/web/views.py
+++ src/web/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from .models import Shop
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    shops = Shop.objects.all()
+    template = loader.get_template('web/index.html')
+    context = {
+        'shops': shops,
+    }
+    return HttpResponse(template.render(context, request))
