2269898343a3d3fc5a2f8a337c27a5bfd7d7923f
Ciaran <>
2023-09-16 22:06:42 +0100

    Load secret from .env file.

--- shipsfrom/settings.py
+++ shipsfrom/settings.py
@@ -11,19 +11,24 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(BASE_DIR / '.env')  # <-- Updated!
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2@b6us9obtosl__%h$p5zyxjuo4)zt$zbd6gg7-0_u2*@t$duz'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["localhost", '127.0.0.1', '.fly.dev']
 CSRF_TRUSTED_ORIGINS = ['https://*.fly.dev', 'http://*.fly.dev']
